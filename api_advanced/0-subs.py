edd9f076c53f083bae4c6b0e5381a3349938b54e
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2024-03-01 08:43:48 -0800

    Made by Daniel

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for the given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
